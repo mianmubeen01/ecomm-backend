25aa275dfb34f3f5075f64117ddcca2750e206aa
Mubeen Tanveer <work.mubeen@gmail.com>
2025-07-06 08:07:46 -0700

    Prepare for Render deployment

--- backend/settings.py
+++ backend/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 INSTALLED_APPS = [
